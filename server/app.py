c99dece2e8e590fe0d5c864e93b8ee0a35613002
wilmwainaina <wilfred.wainaina@student.moringaschool.com>
2023-09-18 10:51:10 +0300

    completed

--- server/app.py
+++ server/app.py
@@ -3,3 +3,18 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+# append to app/app.py
+
+@app.route('/')
+def index():
+    return '<h1>Welcome to my page!</h1>'
+
+# modify user() in server/app.py
+
+@app.route('/<string:username>')
+def user(username):
+    return f'<h1>Profile for {username}</h1>'
+
+if __name__ == '__main__':
+    app.run(port=5555)
